6b83fe32312f8e13322d4a48ea408bc0647f6bce
ookamiyoujyo <ookamiyoujyo@gmail.com>
2019-04-26 15:56:33 +0900

    完成

--- enshu0426/Form1.cs
+++ enshu0426/Form1.cs
@@ -16,5 +16,20 @@ namespace enshu0426
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Blue;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.ForeColor = Color.Pink;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+        }
     }
 }
